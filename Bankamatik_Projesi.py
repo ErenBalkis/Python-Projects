68748194ff0672fda047d137dffd828b10048151
Eren Balkış <erenbalkis7@gmail.com>
2025-06-01 21:17:09 +0300

    Add files via upload

--- Bankamatik_Projesi.py
+++ Bankamatik_Projesi.py
@@ -1,110 +1,110 @@
 import sys
 
-hesap_Bilgileri = {
-    1234: {
-        "Ad":"Hasan",
-        "Soyad":"Yılmaz",
-        "Bakiye":2500,
-        "Sifre":1234
+hesap_Bilgileri = [
+    {
+        "ad":"Hasan Yılmaz",
+        "hesapNo":12345,
+        "bakiye":2500,
+        "ekHesap":1500,
+        "username":"hasanyilmaz",
+        "password":"1234"
     },
-    5678: {
-        "Ad":"Hüseyin",
-        "Soyad":"Çakmak",
-        "Bakiye":2000,
-        "Sifre":5678
+    {
+        "ad":"Eren Balkış",
+        "hesapNo":98765,
+        "bakiye":1600,
+        "ekHesap":400,
+        "username":"erenbalkis",
+        "password":"9876"
     },
-    9123: {
-        "Ad":"Salih",
-        "Soyad":"Korkmaz",
-        "Bakiye":5000,
-        "Sifre":9123
+    {
+        "ad":"Hüseyin Çakmak",
+        "hesapNo":23456,
+        "bakiye":2000,
+        "ekHesap":500,
+        "username":"huseyincakmak",
+        "password":"5678"
+    },
+    {
+        "ad":"Salih Korkmaz",
+        "hesapNo":67891,
+        "bakiye":5000,
+        "ekHesap":2500,
+        "username":"salihkorkmaz",
+        "password":"9123"
     }
-}
-def menu():
-    yonlendir = girdi_Alici(menu)
-    if(yonlendir == 1):
-        paraCekme()
-    elif(yonlendir == 2):
-        bakiyeSorgula()
-    elif(yonlendir == 3):
-        paraYatirma()
-    elif(yonlendir == 0):
-        print("İyi Günler, Görüşmek Üzere!")
-        sys.exit()
+]
 
-def sifreSorgula():
-    sifre = int(input("Lütfen hesabınızın 4 haneli şifresini giriniz: "))
-    while(sifre<1000 or sifre>9999):
-        print("Hatalı giriş!")
-        sifre = int(input("Lütfen 4 haneli şifrenizi tekrar giriniz: "))
-    return sifre
-
-def paraCekme():
-    sifre = sifreSorgula()
-    cekim_Miktari = int(input("Ne kadar para çekmek istiyorsunuz? "))
-    if(hesap_Bilgileri[sifre]["Bakiye"] < cekim_Miktari):
-        print("Yetersiz Bakiye!")
-        print("Ek hesabı kullanmak ister misiniz?")
-        ek_Hesap = girdi_Alici(paraCekme)
-        if(ek_Hesap == 1):
-            ek_Hesap_Kullan()
-        elif(ek_Hesap == 0):
-            print("Para çekim miktarını güncellemek için 1'i,")
-            guncelleme = int(input("Menüye dönmek için 0'ı tuşlayınız: "))
-            while(guncelleme<0 or guncelleme>1):
-                print("Hatalı Giriş Yaptınız!")
-                print("Para çekim miktarını güncellemek için 1'i,")
-                guncelleme = int(input("Menüye dönmek için 0'ı tuşlayınız: "))
-            
-        
-
-        cekim_Miktari = int(input("Ne kadar para çekmek istiyorsunuz? "))
-
-def bakiyeSorgula():
-    hesap_Sorgula()
-    sifre = sifreSorgula()
-    print(hesap_Bilgileri[sifre]["Bakiye"])
-
-def paraYatirma():
+def menu (hesap):
     print()
+    print(f"Merhaba {hesap["ad"]}")
+    print("Bakiye Sorgulama için 1'i,")
+    print("Para çekme için 2'yi,")
+    print("Para yatırma için 3'ü,")
+    islem = input("Çıkış yapmak için 0'ı tuşlayınız: ")
 
-def ek_Hesap_Kullan():
-    print("Ek hesap")
+    if islem == "1":
+        bakiyeSorgula(hesap)
+    elif islem == "2":
+        paraCekme(hesap)
+    elif islem == "3":
+        paraYatirma(hesap)
+    elif islem == "0":
+        print("\t\t İYİ GÜNLER GÖRÜŞMEK ÜZERE!")
+        sys.exit()
+    else:
+        print("Yanlış giriş:")
+    menu(hesap)
 
-def hesap_Sorgula():
-    ad = input("Adınızı giriniz: ")
-    soyad = input("Soyadınızı giriniz:")
-    sifre = sifreSorgula()
-    for i in hesap_Bilgileri:
-        if(hesap_Bilgileri[i]["Ad"]["Soyad"]["Sifre"] == hesap_Bilgileri[i][ad][soyad][sifre]):
-            return sifre
-    print("Hatalı giriş")
+def bakiyeSorgula(hesap):
+    print()
+    print(f"Hesabınızda bulunan bakiye: {hesap["bakiye"]}")
+    print(f"Hesabınızda bulunan ek hesap bakiyesi: {hesap["ekHesap"]}")
 
-def girdi_Alici(fonk):
-    if(fonk == menu):
-        print("Para çekme için 1'i,")
-        print("Bakiye Sorgulama için 2'yi,")
-        print("Para yatırma için 3'ü,")
-        yonlendir = int(input("Çıkış yapmak için 0'ı tuşlayınız:"))
-        while(yonlendir<0 or yonlendir>3):
-            print("")
-            print("                             Hatalı Giriş!")
-            print("Para çekme için 1'i,")
-            print("Bakiye Sorgulama için 2'yi,")
-            print("Para yatırma için 3'ü,")
-            yonlendir = int(input("Çıkış yapmak için 0'ı tuşlayınız:"))
-        return yonlendir
-    
-    if (fonk == paraCekme):
-        ek_Hesap = int(input("Evet için 1'i, Hayır için 0'ı tuşlayınız: "))
-        while(ek_Hesap<0 or ek_Hesap>1):
-            print("Hatalı Giriş Yaptınız!")
-            ek_Hesap = int(input("Lütfen evet için 1'i, Hayır için 0'ı tuşlayınız: "))
-        return ek_Hesap
+def paraCekme(hesap):
+    print()
+    cekim_Mitari = int(input("Çekmek istediğiniz para miktarını giriniz:"))
 
-    # while(guncelleme<0 or guncelleme>1):
-    #     print("Hatalı Giriş Yaptınız!")
-    #     print("Para çekim miktarını güncellemek için 1'i,")
-    #     guncelleme = int(input("Menüye dönmek için 0'ı tuşlayınız: "))
+    if(hesap["bakiye"]>=cekim_Mitari):
+        hesap["bakiye"] -= cekim_Mitari
+        print("Paranızı almayı unutmayın!")
+    else:
+        toplam = hesap["bakiye"] + hesap["ekHesap"]
+        if toplam >= cekim_Mitari:
+            print("Bakiye Yetersiz!")
+            ekHesapIzni = input("Ek hesap kullanılsın mı?(e/h)")
+            if ekHesapIzni == "e":
+                kullanilacakMiktar = cekim_Mitari - hesap["bakiye"]
+                hesap["bakiye"] = 0
+                hesap["ekHesap"] -= kullanilacakMiktar
+                print("Paranızı alabilirsiniz!")
+            else:
+                print("Üzgünüz Bakiyeniz Yetersiz!")
+        else :
+            print("Üzgünüz Bakiyeniz Yetersiz!")
 
-menu()
+def paraYatirma(hesap):
+    print()
+    yatirmaMiktari = int(input("Yatırmak istediğiniz para miktarını giriniz:"))
+    if yatirmaMiktari > 0:
+        hesap["bakiye"] += yatirmaMiktari
+        print("Para hesabınıza eklendi.")
+    else:
+        print("\t\t HATALI GİRİŞ!")
+
+def login():
+    username = input("username: ")
+    password = input("password: ")
+
+    isLoggedIn = False
+
+    for hesap in hesap_Bilgileri:
+        if hesap["username"] == username and hesap["password"] == password:
+            isLoggedIn = True
+            menu(hesap)
+            break
+
+    if not(isLoggedIn):
+        print("username ya da parola yanlış!")
+
+login()
